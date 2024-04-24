a7ab43d35cfb535523e6bb9fb8bf9f511ae311a5
Nina Prus <nina.prus95@gmail.com>
2024-04-24 07:18:07 +0300

    Fix handling of Product not found error in NewReviewCreate view

--- drf_project_test/reviews/views.py
+++ drf_project_test/reviews/views.py
@@ -1,5 +1,6 @@
 
 from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
 from products.models import Product
 from reviews.serializers import NewReviewSerializer
@@ -10,5 +11,8 @@ class NewReviewCreate(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         product_id = self.kwargs.get('product_id')
-        product = Product.objects.get(pk=product_id)
+        try:
+            product = Product.objects.get(pk=product_id)
+        except Product.DoesNotExist:
+            raise NotFound("Product does not exist.")
         serializer.save(product=product)
